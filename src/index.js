0345a600e34abb078b6c37f08007f1f5b76793e8
judah <87juda@gmail.com>
2022-04-09 04:52:14 -0300

    Remove comments on index.js file

--- src/index.js
+++ src/index.js
@@ -6,22 +6,3 @@ const scoreBoard = new ScoreBoard();
 scoreBoard.paintToPage();
 scoreBoard.setUpAddListener();
 scoreBoard.setInputFieldListeners();
-
-// TESTS PROMISES
-
-// console.log('Hi friend');
-
-// const myPromise = new Promise((resolve, reject) => {
-//   const connection = false;
-//   if (connection) {
-//     resolve('connection established');
-//   } else {
-//     reject();
-//   }
-// });
-
-// myPromise.then((message) => {
-//   console.log(message);
-// }).catch((message) => {
-//   console.log(message);
-// });
